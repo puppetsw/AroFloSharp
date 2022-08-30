3c5f0ea46b057f34679420514c793c8640da3ccb
Scott Whitney <79826944+puppetsw@users.noreply.github.com>
2022-08-31 07:29:16 +0930

    Test cleanup

--- tests/AroFloSharp.Tests/RequestMessageTests.cs
+++ tests/AroFloSharp.Tests/RequestMessageTests.cs
@@ -5,55 +5,54 @@ using AroFloSharp.Client.Enums;
 using AroFloSharp.Client.Parameters;
 using NUnit.Framework;
 
-namespace AroFloSharp.Tests
+namespace AroFloSharp.Tests;
+
+[TestFixture]
+public class RequestMessageTests
 {
-    [TestFixture]
-    public class RequestMessageTests
+    [Test]
+    public async Task Test_request_message_xml()
     {
-        [Test]
-        public async Task Test_request_message_xml()
+        using var client = new AroFloSharpClient(options =>
         {
-            using var client = new AroFloSharpClient(options =>
-            {
-                options.Timeout = TimeSpan.FromSeconds(5);
-                options.DataFormat = DataFormat.Xml;
-                options.SecretKey = Credentials.SECRET_KEY;
-                options.UEncode = Credentials.U_ENCODE;
-                options.PEncode = Credentials.P_ENCODE;
-                options.OrgEncode = Credentials.ORG_ENCODE;
-            });
-            var data = await client.GetResponseAsync(parameters =>
-            {
-                parameters.Add(new ZoneParameter(AroFloZone.Projects));
-                parameters.Add(new PageParameter(1));
-            });
-
-            Console.WriteLine(data);
-            Assert.IsTrue(client.Status == AroFloStatus.LoginOk);
-            Assert.IsTrue(data != "");
-        }
-
-        [Test]
-        public async Task Test_request_message_json()
+            options.Timeout = TimeSpan.FromSeconds(5);
+            options.DataFormat = DataFormat.Xml;
+            options.SecretKey = Credentials.SECRET_KEY;
+            options.UEncode = Credentials.U_ENCODE;
+            options.PEncode = Credentials.P_ENCODE;
+            options.OrgEncode = Credentials.ORG_ENCODE;
+        });
+        var data = await client.GetResponseAsync(parameters =>
         {
-            using var client = new AroFloSharpClient(options =>
-            {
-                options.Timeout = TimeSpan.FromSeconds(5);
-                options.DataFormat = DataFormat.Json;
-                options.SecretKey = Credentials.SECRET_KEY;
-                options.UEncode = Credentials.U_ENCODE;
-                options.PEncode = Credentials.P_ENCODE;
-                options.OrgEncode = Credentials.ORG_ENCODE;
-            });
-            var data = await client.GetResponseAsync(parameters =>
-            {
-                parameters.Add(new ZoneParameter(AroFloZone.Projects));
-                parameters.Add(new PageParameter(1));
-            });
+            parameters.Add(new ZoneParameter(AroFloZone.Projects));
+            parameters.Add(new PageParameter(1));
+        });
 
-            Console.WriteLine(data);
-            Assert.IsTrue(client.Status == AroFloStatus.LoginOk);
-            Assert.IsTrue(data != "");
-        }
+        Console.WriteLine(data);
+        Assert.IsTrue(client.Status == AroFloStatus.LoginOk);
+        Assert.IsTrue(data != "");
+    }
+
+    [Test]
+    public async Task Test_request_message_json()
+    {
+        using var client = new AroFloSharpClient(options =>
+        {
+            options.Timeout = TimeSpan.FromSeconds(5);
+            options.DataFormat = DataFormat.Json;
+            options.SecretKey = Credentials.SECRET_KEY;
+            options.UEncode = Credentials.U_ENCODE;
+            options.PEncode = Credentials.P_ENCODE;
+            options.OrgEncode = Credentials.ORG_ENCODE;
+        });
+        var data = await client.GetResponseAsync(parameters =>
+        {
+            parameters.Add(new ZoneParameter(AroFloZone.Projects));
+            parameters.Add(new PageParameter(1));
+        });
+
+        Console.WriteLine(data);
+        Assert.IsTrue(client.Status == AroFloStatus.LoginOk);
+        Assert.IsTrue(data != "");
     }
 }
